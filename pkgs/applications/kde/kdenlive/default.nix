a94109a7f59b61e40bc566dd2c4dd54a4e82097f
Drewry Pope <drewrypope@gmail.com>
2024-08-11 00:56:55 -0500

    Revert "kdenlive: make glaxnimate non-optional"
    This reverts commit cc9be80fec92adeebdd9f878880a0805201c34df.

--- pkgs/applications/kde/kdenlive/default.nix
+++ pkgs/applications/kde/kdenlive/default.nix
@@ -35,6 +35,7 @@
 , kdeclarative
 , wrapGAppsHook3
 , glaxnimate
+, enableGlaxnimate ? true
 }:
 
 let
@@ -81,12 +82,11 @@ mkDerivation {
     kpurpose
     kdeclarative
     wrapGAppsHook3
-    glaxnimate
-  ];
+  ] ++ lib.optional enableGlaxnimate glaxnimate;
   # Both MLT and FFMpeg paths must be set or Kdenlive will complain that it
   # doesn't find them. See:
   # https://github.com/NixOS/nixpkgs/issues/83885
-  patches = [ ./dependency-paths.patch ./dependency-paths-glaxnimate.patch ];
+  patches = [ ./dependency-paths.patch ] ++ lib.optional enableGlaxnimate ./dependency-paths-glaxnimate.patch;
 
   # Needed to replace @glaxnimate@ by its path
   env = {
