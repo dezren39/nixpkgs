5ac3e759b1b91b787e4a9e6d50d941acca928816
Drewry Pope <drewrypope@gmail.com>
2024-09-02 09:21:42 -0500

    main

--- code.sh
+++ code.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 git pull --all
-GIT_MERGE_AUTOEDIT=no git merge nixpkgs/master
+GIT_MERGE_AUTOEDIT=no git merge nixpkgs/main
 git push
 cd ~/code
 ./auth-rebuild.sh
